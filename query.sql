c8a438e875124fadc7f200069d9e425444f0f470
DovIra <44786442+DovIra@users.noreply.github.com>
2020-05-18 12:08:41 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,18 +1,19 @@
 -- query1
 select
 LOCATIONS.LOCATION_NAME, sum(RAINFALL) as sum_rainfall
-from WEATHER
-join LOCATIONS on LOCATIONS.LOCATION_NAME = WEATHER.location_code
+from WEATHER_DAILY
+join LOCATIONS on LOCATIONS.LOCATION_NAME = WEATHER_DAILY.location_code
 group by LOCATION_CODE
 order by sum(RAINFALL) desc
-FETCH FIRST 5 ROWS ONLY
-;
+FETCH FIRST 5 ROWS ONLY;
+
 -- query2
 select LOCATIONS.LOCATION_NAME, sum(RAINFALL) as sum_rainfall
-from WEATHER
-join LOCATIONS on LOCATIONS.LOCATION_NAME = WEATHER.location_code
+from WEATHER_DAILY
+join LOCATIONS on LOCATIONS.LOCATION_NAME = WEATHER_DAILY.location_code
 group by LOCATION_CODE
 order by 2 desc
+FETCH FIRST 10 ROWS ONLY
 ;
 
 -- query3
@@ -31,6 +32,6 @@ from (
                  WEATHER_DAILY.WEATHER_DATE = WEATHER_HOURLY.WEATHER_DATE
          GROUP BY WEATHER_DAILY.LOCATION_CODE, WEATHER_DAILY.WEATHER_DATE
      )
-where extract(year from WEATHERDATE) = 2017
+where extract(year from WEATHERDATE) = 2007
 group by to_char(WEATHERDATE,'Month'), extract(month from WEATHERDATE)
 order by extract(month from WEATHERDATE)
